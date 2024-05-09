fbb072f4afc56c987fbbf008468ce1c303bb6828
Raquel <raquelcosta5@gmail.com>
2024-05-09 00:39:40 -0300

    moniBank

---
+++ pages/js/valida-idade.js
@@ -0,0 +1,18 @@
+export default function ehMaiorDeIdade(campo) {
+  const dataNascimento = new Date(campo.value)
+ if(!validaIdade(dataNascimento)){
+  campo.setCustomValidity('O usuario não e maior de iadade')
+ }
+
+}
+
+function validaIdade(data) {
+  const dataAtual = new Date()
+  const dataMais18 = new Date(
+    data.getUTCFullYear() + 18,
+    data.getUTCMonth(),
+    data.getUTCDate()
+  )
+
+  return dataAtual >= dataMais18
+}
